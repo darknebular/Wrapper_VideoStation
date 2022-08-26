ed4135197aa79db42daf0beb06a8892595da3913
darknebular <82757922+darknebular@users.noreply.github.com>
2022-08-26 12:21:44 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -192,7 +192,18 @@ function uninstall() {
 
 function configurator() {
    info "${BLUE}==================== Configuration: Start ===================="
-   
+   info "${BLUE}Actually you have two audio streams, first MP3 2.0 256kbps and second 5.1 AAC 512kbps."
+   info "${YELLOW}REMEMBER: If you change the order you will have ALWAYS AAC 5.1 512kbps in first audio stream in VideoStation and DLNA and some devices not compatibles with 5.1 neigther multi audio streams like Chromecast won't work"
+   echo -e "${GREEN}1) FIRST STREAM= AAC 5.1 512kbps, SECOND STREAM= MP3 2.0 256kbps" 
+   echo -e "${GREEN}2) FIRST STREAM= MP3 2.0 256kbpss, SECOND STREAM= AAC 5.1 512kbps" 
+	while true; do
+        read -p "Do you wish to change the order of these audio stream in the actual wrapper? " 12
+        case $yn in
+        1 ) uninstall_old; break;;
+        2 ) exit;;
+        * ) echo "Please answer with the correct option writing: 1 or 2.";;
+        esac
+        done
    
    info "${BLUE}==================== Configuration: Complete ===================="
 }
