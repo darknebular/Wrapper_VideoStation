2c8f2cd48e9de8d15c449c2feda8826ef0719a8a
darknebular <82757922+darknebular@users.noreply.github.com>
2022-08-28 01:08:21 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ version="SCPT_1.7"
 # SCPT_1.4: Fixed a bug: when you select simplest_wrapper with only MP3 2.0 and then try to change the order of the audio codecs you will have a error. (Deprecated migrated to SCPT_1.5)
 # SCPT_1.5: Fixed a bug: when you have a low connection to Internet that could have problems. (Deprecated migrated to SCPT_1.6)
 # SCPT_1.6: Added a independent audio´s streams for DLNA. (Deprecated migrated to SCPT_1.7)
-# SCPT_1.7: Added a independent installer for simplest_wrapper in MAIN menu. Added new configuration options in configurator_menu. Now you can change from AAC to AC3. 
+# SCPT_1.7: Added a independent installer for simplest_wrapper in MAIN menu. Added new configuration options in configurator_menu. Now you can change from AAC 512kbps to AC3 640kbps and vice versa. 
 
 ###############################
 # VARIABLES
