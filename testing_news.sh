9ed5a6360dc45ea776f91cf223df5ccc18fe5720
darknebular <82757922+darknebular@users.noreply.github.com>
2022-08-28 20:09:20 +0200

    Update testing_news.sh

--- testing_news.sh
+++ testing_news.sh
@@ -7,14 +7,5 @@ version="SCPT_1.8"
 # VARIABLES
 ###############################
 
-repo_url="https://raw.githubusercontent.com/darknebular/Wrapper_VideoStation"
-setup="start"
-dependencias=("VideoStation" "ffmpeg" "CodecPack" "MediaServer")
-RED="\u001b[31m"
-BLUE="\u001b[36m"
-PURPLE="\u001B[35m"
-GREEN="\u001b[32m"
-YELLOW="\u001b[33m"
-supported_versions=("7.0" "7.1")
-injector="1-12.3.3"
+
 
