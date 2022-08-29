309a93177a3e62b064de836f6d49b3127fc7d4d4
darknebular <82757922+darknebular@users.noreply.github.com>
2022-08-29 17:03:24 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -68,7 +68,7 @@ function restart_packages() {
 function check_dependencias() {
   for dependencia in "${dependencias[@]}"; do
     if [[ ! -d "/var/packages/$dependencia" ]]; then
-      error "MISSING $dependencia Package, please Install It and re-run the Installer again."
+      error "MISSING $dependencia Package, please Install It and RE-RUN the Installer again."
       exit 1
     fi
   done
