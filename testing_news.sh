6ac5cab018cd3f352df27a489455e78d80c06da2
darknebular <82757922+darknebular@users.noreply.github.com>
2022-08-29 10:51:14 +0200

    Update testing_news.sh

--- testing_news.sh
+++ testing_news.sh
@@ -555,7 +555,7 @@ echo ""
 echo ""
 
 if [[ $EUID -ne 0 ]]; then
-  error "YOU MUST BE ROOT FOR EXECUTE THIS INSTALLER. Please write (echo -e "${PURPLE}" sudo -i echo -e "${RED}") and try again with the installer."
+  error "YOU MUST BE ROOT FOR EXECUTE THIS INSTALLER. Please write ("${PURPLE}" sudo -i "${RED}") and try again with the installer."
   exit 1
 fi
 
