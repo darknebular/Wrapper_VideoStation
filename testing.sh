ccc78695b0ed1f9525566cd6ea31ca3fbc0718f0
darknebular <82757922+darknebular@users.noreply.github.com>
2022-11-17 18:00:25 +0100

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##############################################################
-version="SCPT_2.6alpha2"
+version="SCPT_2.6alpha3"
 # Changes:
 # SCPT_1.X: See these changes in the releases notes in my Repository in Github. (Deprecated)
 # SCPT_2.0: Initial new major Release. Clean the code from last versions. (Deprecated migrated to SCPT_2.1)
