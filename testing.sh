2b43c2b014c4ac1e02112c9a0271f236854fe3b6
darknebular <82757922+darknebular@users.noreply.github.com>
2022-11-18 10:27:17 +0100

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1091,7 +1091,7 @@ fi
 
 
 if [[ "$unmode" == "New" ]]; then
-  if [[ -f "$cp_bin_path/ffmpeg41.orig" ]]; then
+  if [[ -f "/tmp/wrapper.KEY" ]]; then
   info "${BLUE}${text_uninstall_13[$LANG]}"
   
   info "${YELLOW}${text_uninstall_2[$LANG]}"
