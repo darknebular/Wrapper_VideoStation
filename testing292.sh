977b0ed3fb9bd481f890d256bd4355a52848b0c6
darknebular <82757922+darknebular@users.noreply.github.com>
2023-05-10 13:18:10 +0200

    Update testing292.sh

--- testing292.sh
+++ testing292.sh
@@ -18,7 +18,7 @@ version="SCPT_3.1"
 dsm_version=$(cat /etc.defaults/VERSION | grep productversion | sed 's/productversion=//' | tr -d '"')
 repo_url="https://raw.githubusercontent.com/darknebular/Wrapper_VideoStation"
 setup="start"
-dependencias=("VideoStation" "ffmpeg" "CodecPack")
+dependencias=("VideoStation" "ffmpeg6" "CodecPack")
 RED="\u001b[31m"
 BLUE="\u001b[36m"
 PURPLE="\u001B[35m"
