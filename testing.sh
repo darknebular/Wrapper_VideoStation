1c98952db993e282563776598e00ac3c55cb79a8
darknebular <82757922+darknebular@users.noreply.github.com>
2023-05-21 09:49:00 +0200

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##############################################################
-version="SCPT_3.4.BETA.1"
+version="SCPT_3.4.BETA"
 # Changes:
 # SCPT_1.X: See these changes in the releases notes in my Repository in Github. (Deprecated)
 # SCPT_2.X: See these changes in the releases notes in my Repository in Github. (Deprecated)
