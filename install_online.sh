7a2430853a517ba180322b29ce5afeb44fae966b
darknebular <82757922+darknebular@users.noreply.github.com>
2023-05-22 19:17:23 +0200

    Update install_online.sh

--- install_online.sh
+++ install_online.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Download the script called testing128.sh in /tmp/
-curl -o /tmp/testing128.sh "https://raw.githubusercontent.com/darknebular/Wrapper_VideoStation/main/testing128.sh"
+curl -o /tmp/installer_OffLine.sh "https://raw.githubusercontent.com/darknebular/Wrapper_VideoStation/main/installer_OffLine.sh"
 
 # Give execution permissions to the downloaded script
-chmod +x /tmp/testing128.sh
+chmod +x /tmp/installer_OffLine.sh
 
 # Run testing128.sh passing the supplied arguments to the install_online.sh script
-/bin/bash /tmp/testing128.sh "$@"
+/bin/bash /tmp/installer_OffLine.sh "$@"
 
 # Delete the downloaded script
-rm /tmp/testing128.sh
+rm /tmp/installer_OffLine.sh
