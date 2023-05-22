17220edcd56b34e4b1eed4eec6c269f7b37ce297
darknebular <82757922+darknebular@users.noreply.github.com>
2023-05-22 19:22:34 +0200

    Update and rename install_online.sh to installer.sh

--- installer.sh
+++ installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Download the script called testing128.sh in /tmp/
+# Download the script called installer_OffLine.sh in /tmp/
 curl -o /tmp/installer_OffLine.sh "https://raw.githubusercontent.com/darknebular/Wrapper_VideoStation/main/installer_OffLine.sh"
 
 # Give execution permissions to the downloaded script
 chmod +x /tmp/installer_OffLine.sh
 
-# Run testing128.sh passing the supplied arguments to the install_online.sh script
+# Run installer_OffLine.sh passing the supplied arguments to the installer.sh script
 /bin/bash /tmp/installer_OffLine.sh "$@"
 
 # Delete the downloaded script
